fd3ef78ca7fff7dfaf9afecb2925419c1bc74a8a
Persiyan93 <persiyan.georgiev@gmail.com>
2020-12-15 02:45:49 +0200

    init my app

---
+++ Smart-home/Smart-home/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+
+namespace Smart_home
+{
+    public class Program
+    {
+        static SerialPort serialPort;
+        static void Main(string[] args)
+
+        {
+            serialPort = new SerialPort();
+        }
+    }
+}
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace test
+{
+    class Program
+    {
+        static SerialPort serialPort;
+        static void Main(string[] args)
+        {
+            serialPort = new SerialPort("COM3", 9600);
+            serialPort.ReadTimeout = 1500;
+            serialPort.WriteTimeout = 1500;
+            serialPort.Open();
+
+            while (true)
+            {
+                Read();
+            }
+
+        }
+        static void Read()
+        {
+            try
+            {
+                string message = serialPort.ReadLine();
+                Console.WriteLine(message);
+
+            }
+            catch (Exception)
+            {
+                serialPort.Close();
+                Console.Write("exception");
+                return;
+            }
+
+        }
+    }
+}
+
+
+
+
+
